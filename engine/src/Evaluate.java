1828182104a60a4f93e84070f1a5592775f37233
SIGABA <leongehrke@protonmail.com>
2023-05-11 10:57:35 +0200

    efficiency

--- engine/src/Evaluate.java
+++ engine/src/Evaluate.java
@@ -32,62 +32,55 @@ public class Evaluate {
 
     public static void pawn(char[][] board, int r, int f) {
         double n = 0;
+        char current;
+        char opponent;
+        int m = 1;
 
         if (board[r][f] == 'P') {
+            current = 'P';
+            opponent = 'p';
+            m = 1;
+        } else {
+            current = 'p';
+            opponent = 'P';
+            m = -1;
+        }
+
+
             // passed
-            if (checkFile(board, f, 'p') == 0) {
-                if (f == 0 || checkFile(board, f - 1, 'p') == 0) {
-                    if (f == 7 || checkFile(board, f + 1, 'p') == 0) {
-                        System.out.println(board[r][f]);
-                        E += (value_passedPawn * 18) / (8 - (r + 1));
-                        System.out.println(E);
+            if (checkFile(board, f, opponent) == 0) {
+                if (f == 0 || checkFile(board, f - 1, opponent) == 0) {
+                    if (f == 7 || checkFile(board, f + 1, opponent) == 0) {
+                        if (current == 'P'){
+                            E += (value_passedPawn * 18) / (8 - (r + 1));
+                        } else {
+                            E -= (value_passedPawn * 18) /r;
+                        }
                     }
                 }
             }
 
             // isolated white
-            if (f == 0 || checkFile(board, f - 1, 'P') == 0) {
-                if (f == 7 || checkFile(board, f + 1, 'P') == 0) {
-                    E -= value_isolatedPawn;
+            if (f == 0 || checkFile(board, f - 1, current) == 0) {
+                if (f == 7 || checkFile(board, f + 1, current) == 0) {
+                    E -= value_isolatedPawn * m;
                 }
             }
 
             // stacked white
-            n = checkFile(board, f, 'P') - 1;
-            E -= (n * value_stackedPawn + ((n*(n-1))/2) * 0.2 * value_stackedPawn)/2;
+            n = checkFile(board, f, current) - 1;
+            System.out.println(f+1+" "+(r+1)+" | "+n+" | "+board[r][f]);
+            E -= ((n * value_stackedPawn + ((n*(n-1))/2) * 0.2 * value_stackedPawn)/2) * m;
             // advanced white
             // chain white
-
-        }
-        if (board[r][f] == 'p') {
-            // passed black
-            if (checkFile(board, f, 'P') == 0) {
-                if (f == 0 || checkFile(board, f - 1, 'P') == 0) {
-                    if ((f == 7 || checkFile(board, f + 1, 'P') == 0)) {
-                        System.out.println(board[r][f]);
-                        E -= (value_passedPawn * 18) /r;
-                        System.out.println(E);
-                    }
-                }
-            }
-            // isolated black
-            if (f == 0 || checkFile(board, f - 1, 'p') == 0) {
-                if (f == 7 || checkFile(board, f + 1, 'p') == 0) {
-                    E += value_isolatedPawn;
-                }
-            }
-            // stacked black
-            n = checkFile(board, f, 'p') - 1;
-            E += (n * value_stackedPawn + ((n*(n-1))/2) * 0.2 * value_stackedPawn)/2;
-            // advanced black
-            // chain black
-        }
     }
 
 
     public static void check(char[][] board, int r, int f) {
 
-        pawn(board, r, f);
+        if (Character.toLowerCase(board[r][f]) == 'p'){
+            pawn(board, r, f);
+        }
 
     }
 }
